14493e67e5aaa9fbd33bbbca3ee292832a331a44
Daniel Chong <danchong625@gmail.com>
2018-02-22 22:56:39 -0800

    [ops] initializes timeslot model

---
+++ src/database/model/timeslot.js
@@ -0,0 +1,33 @@
+import Sequelize from 'sequelize';
+
+import { db } from '../index';
+import { User } from './user';
+import { Room } from './room';
+
+const Timeslot = db.define('Timeslot', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  start: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+  end: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+  finished: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+});
+
+Timeslot.belongsTo(User, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' });
+Timeslot.belongsTo(Room, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' });
+
+User.hasMany(Timeslot);
+Room.hasMany(Timeslot);
+
+export { Timeslot };
