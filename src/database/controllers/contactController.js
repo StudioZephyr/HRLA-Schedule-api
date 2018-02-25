c52a60fefa8c0d84bba723cdb730f82166f74ac6
Daniel Chong <danchong625@gmail.com>
2018-02-25 15:36:33 -0800

    [ops] creates method to get all contacts

--- src/database/controllers/contactController.js
+++ src/database/controllers/contactController.js
@@ -2,4 +2,24 @@ import Promise from 'bluebird';
 
 import { Contact } from '../model/contact';
 
-export {};
+const getAllContacts = (id) => {
+  return new Promise((resolve, reject) => {
+    Contact.findAll({ where: { UserId: id } })
+      .then(contacts => {
+        const contactArr = [];
+        for (let i = 0; i < contacts.length; i++) {
+          contactList.push(contacts[i].dataValues);
+        }
+        resolve(contactArr);
+      })
+      .catch(err => {
+        console.log(`Error grabbing all Contacts for UserId: ${id}. Error: ${err}`);
+        reject({
+          message: `Error grabbing all Contacts`,
+          error: true,
+        });
+      });
+  });
+};
+
+export { getAllContacts };
