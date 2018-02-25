0aa93dbcec04bcea226ad17e19e32487575eca3a
Daniel Chong <danchong625@gmail.com>
2018-02-25 15:42:09 -0800

    [ops] creates and implements method to get all user data

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,3 +1,30 @@
-import {} from '../database/controllers/userController';
+import { 
+  getAllUsers 
+} from '../database/controllers/userController';
 
-export {};
+import {
+  getAllContacts
+} from '../database/controllers/contactController';
+
+const getAllLogins = (req, res) => {
+  getAllUsers()
+    .then(async (users) => {
+      const userArr = [];
+      for (let i = 0; i < users.length; i++) {
+        await getAllContacts(users[i].id)
+          .then(contact => {
+            users[i].contacts = data;
+            userObj.push(users[i]);
+          })
+          .catch(err => {
+            console.log(`Error: ${err.message}`);
+          });
+      }
+      res.status(200).send({ result: userObj });
+    })
+    .catch(err => {
+      res.status(500).send({ error: err });
+    });
+};
+
+export { getAllLogins };
--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,7 @@ import express from 'express';
 
 const Router = express.Router();
 
+Router.route('/login')
+  .get(getAllLogins);
+
 export { Router };
