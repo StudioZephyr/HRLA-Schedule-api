115276862e7f1c4d2a1749de6f062d661f3ad61c
Daniel Chong <danchong625@gmail.com>
2018-02-25 15:27:47 -0800

    [ops] adds method to grab all users from db

--- src/database/controllers/userController.js
+++ src/database/controllers/userController.js
@@ -2,4 +2,24 @@ import Promise from 'bluebird';
 
 import { User } from '../model/user';
 
-export {};
+const getAllUsers = () => {
+  return new Promise((resolve, reject) => {
+    User.findAll()
+      .then(users => {
+        const userArr = [];
+        for (let i = 0; i < users.length; i++) {
+          userArr.push(users[i].dataValues);
+        }
+        resolve(userArr);
+      })
+      .catch(err => {
+        console.log(`Error grabbing all Users. Error: ${err}`);
+        reject({
+          message: `Error grabbing all Users`,
+          error: true,
+        });
+      });
+  });
+};
+
+export { getAllUsers };
