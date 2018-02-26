f48832f10c70b916f6e29b4fd17b0f3d23a84b94
Daniel Chong <danchong625@gmail.com>
2018-02-25 17:24:28 -0800

    [ops] creates new method to add contact to db

--- src/database/controllers/contactController.js
+++ src/database/controllers/contactController.js
@@ -22,4 +22,21 @@ const getAllContacts = (id) => {
   });
 };
 
-export { getAllContacts };
+const addContact = (contactObj, UserId) => {
+  const contactInfo = Object.assign({}, contactObj, { UserId });
+  return new Promise((resolve, reject) => {
+    Contact.create(contactInfo)
+      .then(() => {
+        resolve(`Contact Added`);
+      })
+      .catch(err => {
+        console.log(`Error creating contact. Error: ${err}`);
+        reject({
+          message: `Error creating contact`,
+          contact: false,
+        });
+      });
+  });
+};
+
+export { getAllContacts, addContact };
